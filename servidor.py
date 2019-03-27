ccf145f93d9a719ec5af879d317bd285a14ac2ef
LuanaChiodini <lchiodini87@gmail.com>
2019-03-27 14:22:00 -0300

    Uhuu deu certo!

--- servidor.py
+++ servidor.py
@@ -6,13 +6,8 @@ app = Flask(__name__)
 def index():
 	return render_template("pagina_inicial.html")
 
-@app.route("/cadastro", methods=["GET", "POST"])
+@app.route("/cadastro")
 def cadastrar():
-	if request.method == "POST":
-		nome = request.form["nome"]
-		email = request.form["email"]
-		senha = request.form["senha"]
-		pais = request.form["pais"]
 	return render_template("cadastro.html")
 
 app.run(debug=True, host="0.0.0.0")
