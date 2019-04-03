ff87686b1a5786d4c8668391a0196d8af8ffdd31
LuanaChiodini <lchiodini87@gmail.com>
2019-04-03 13:47:15 -0300

    arrumando categorias

--- servidor.py
+++ servidor.py
@@ -4,10 +4,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return render_template("pagina_inicial.html")
+	return render_template("index.html")
 
 @app.route("/cadastro")
 def cadastrar():
 	return render_template("cadastro.html")
+
+@app.route("/sobre")
+def sobre():
+	return render_template("sobre.html")
+
+@app.route("/categorias")
+def categorias():
+	return render_template("categorias.html")
 
 app.run(debug=True, host="0.0.0.0")
